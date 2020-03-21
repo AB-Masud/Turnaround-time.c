11dff1791d589a7d0f6c980cca35d474c957ca3e
AB-Masud <62434554+AB-Masud@users.noreply.github.com>
2020-03-21 17:04:43 +0530

    Update turnaround-time.c

--- turnaround-time.c
+++ turnaround-time.c
@@ -1,1 +1,8 @@
-
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int burst_time[10],process[10],n,temp,i,j,waiting_time[10],sum=0;
+    float average;
+    printf("enter total number of process: );
+    printf("enter burst time for each process: );
